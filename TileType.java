3cac11721f9d3429882f4baa2a5b4470c8625ace
bbomberger <bomberger.benjamin@gmail.com>
2015-04-21 13:16:16 -0500

    Initial commit

---
+++ TileType.java
@@ -0,0 +1,17 @@
+package sssnake;
+
+/**
+ * The {@code TileType} class represents the different
+ * types of tiles that can be displayed on the screen.
+ * @author Brendan Jones
+ *
+ */
+public enum TileType {
+
+	Mouse,
+	
+	SnakeHead,
+	
+	SnakeBody
+	
+}
